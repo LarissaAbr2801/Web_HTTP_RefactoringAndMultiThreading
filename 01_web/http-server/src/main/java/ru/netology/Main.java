e4e9a5bf336990ba2703307000356da4f72c2f94
LarissaAbr2801 <abramova.la.28@gmail.com>
2022-06-24 18:56:07 +0300

    first commit

--- 01_web/http-server/src/main/java/ru/netology/Main.java
+++ 01_web/http-server/src/main/java/ru/netology/Main.java
@@ -1,82 +1,33 @@
 package ru.netology;
 
-import java.io.*;
-import java.net.ServerSocket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
+
+  static final int PORT = 9999;
+  static final int THREADS_QUANTITY = 4;
+
   public static void main(String[] args) {
-    final var validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+    ExecutorService executorService = Executors.newFixedThreadPool(THREADS_QUANTITY);
 
-    try (final var serverSocket = new ServerSocket(9999)) {
-      while (true) {
-        try (
-            final var socket = serverSocket.accept();
-            final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            final var out = new BufferedOutputStream(socket.getOutputStream());
-        ) {
-          // read only request line for simplicity
-          // must be in form GET /path HTTP/1.1
-          final var requestLine = in.readLine();
-          final var parts = requestLine.split(" ");
+    final var validPaths = new CopyOnWriteArrayList<>(Arrays.asList("/index.html", "/spring.svg", "/spring.png", "/resources.html",
+            "/styles.css", "/app.js", "/links.html", "/forms.html",
+            "/classic.html", "/events.html", "/events.js"));
 
-          if (parts.length != 3) {
-            // just close socket
-            continue;
-          }
+    String classicCasePath = "/classic.html";
+    String pathName = "01_web/http-server/public";
 
-          final var path = parts[1];
-          if (!validPaths.contains(path)) {
-            out.write((
-                "HTTP/1.1 404 Not Found\r\n" +
-                    "Content-Length: 0\r\n" +
-                    "Connection: close\r\n" +
-                    "\r\n"
-            ).getBytes());
-            out.flush();
-            continue;
-          }
+    Server server = new Server(validPaths, PORT, pathName, classicCasePath);
+    server.start();
 
-          final var filePath = Path.of(".", "public", path);
-          final var mimeType = Files.probeContentType(filePath);
-
-          // special case for classic
-          if (path.equals("/classic.html")) {
-            final var template = Files.readString(filePath);
-            final var content = template.replace(
-                "{time}",
-                LocalDateTime.now().toString()
-            ).getBytes();
-            out.write((
-                "HTTP/1.1 200 OK\r\n" +
-                    "Content-Type: " + mimeType + "\r\n" +
-                    "Content-Length: " + content.length + "\r\n" +
-                    "Connection: close\r\n" +
-                    "\r\n"
-            ).getBytes());
-            out.write(content);
-            out.flush();
-            continue;
-          }
-
-          final var length = Files.size(filePath);
-          out.write((
-              "HTTP/1.1 200 OK\r\n" +
-                  "Content-Type: " + mimeType + "\r\n" +
-                  "Content-Length: " + length + "\r\n" +
-                  "Connection: close\r\n" +
-                  "\r\n"
-          ).getBytes());
-          Files.copy(filePath, out);
-          out.flush();
-        }
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+    for (int i =0; i < THREADS_QUANTITY; i++) {
+      executorService.submit(new Thread(server::process));
     }
+    executorService.shutdown();
+
   }
 }
 
